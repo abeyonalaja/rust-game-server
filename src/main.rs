c0d1ee010c2b5218ead673c8748836b8bc2fbf7f
abey onalaja <abey.onalaja@gmail.com>
2023-01-01 18:59:01 +0000

    Add /game websocket route

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,51 @@
 use glam::Vec2;
+use serde::{Deserialize, Serialize};
+use warp::ws::WebSocket;
 use warp::Filter;
 
-#[derive(Clone)]
+#[derive(Clone, Deserialize, Serialize)]
 pub struct State {
     pub pos: Vec2,
     pub r: f32,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Deserialize, Serialize)]
 pub struct RemoteState {
     pub id: usize,
     pub position: Vec2,
     pub rotation: f32,
 }
 
+#[derive(Deserialize, Serialize)]
 pub enum ServerMessage {
     Welcome(usize),
     GoodBye(usize),
     Update(Vec<RemoteState>),
 }
 
+#[derive(Deserialize, Serialize)]
 pub enum ClientMessage {
     State(State),
+}
+
+async fn user_connected(ws: WebSocket) {
+    unimplemented!();
 }
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
+    let game = warp::path("game")
+        .and(warp::ws())
+        .map(|ws: warp::ws::Ws| ws.on_upgrade(move |socket| user_connected(socket)));
+
     let status = warp::path!("status").map(move || warp::reply::html("Hello"));
-    warp::serve(status).run(([0, 0, 0, 0], 3030)).await
+
+    let routes = status.or(game);
+    warp::serve(routes).run(([0, 0, 0, 0], 3030)).await
+}
+
+async fn send_msg(msg: ServerMessage) {
+    let buffer = serde_json::to_vec(&msg).unwrap();
 }
