14a714be4a212933bad4c2d767c118ea15cbf0f0
abey onalaja <abey.onalaja@gmail.com>
2023-01-01 18:44:09 +0000

    Server message types

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
+use glam::Vec2;
 use warp::Filter;
+
+#[derive(Clone)]
+pub struct State {
+    pub pos: Vec2,
+    pub r: f32,
+}
+
+#[derive(Clone)]
+pub struct RemoteState {
+    pub id: usize,
+    pub position: Vec2,
+    pub rotation: f32,
+}
+
+pub enum ServerMessage {
+    Welcome(usize),
+    GoodBye(usize),
+    Update(Vec<RemoteState>),
+}
+
+pub enum ClientMessage {
+    State(State),
+}
 
 #[tokio::main]
 async fn main() {
